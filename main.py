e098cb097993398b70ba498339a20a1486df8b3e
Mark Kovalev <restonich@gmail.com>
2021-07-25 21:52:23 +0300

    Minimal packet filtering in TC program

--- main.py
+++ main.py
@@ -1,73 +1,94 @@
 #!/usr/bin/python
+
 import argparse
 import socket
+import pyroute2
 
-import tc_bpf_prog
+from tc_bpf_prog import tc_generate_and_load
 
 # Abcent from socket
 ETH_P_IP = 0x0800
 
-def parse_filter(args):
+def parse_tc_filter(args):
 	'''
 	Args:
 		args (str): 
 
 	Returns:
-		filter (dict):
+		tc_filter (dict):
+			'link'
 			'proto'
 			'src'
 			'dst'
 			'port'
 	'''
-	filter = {}
+	tc_filter = {}
 
-	filter['proto'] = {
+	ipr = pyroute2.IPRoute()
+	link = ipr.link_lookup(ifname=args.link)
+	if not link:
+		print(f"Interface is not found: {args.link}")
+		return None
+	tc_filter['link'] = link[0]
+
+	tc_filter['proto'] = {
 		'icmp' : socket.IPPROTO_ICMP,
 		'tcp'  : socket.IPPROTO_TCP,
 		'udp'  : socket.IPPROTO_UDP
 	}[args.proto]
 
 	try:
-		filter['src'] = int.from_bytes(socket.inet_pton(socket.AF_INET, args.src), 'big')
-		filter['dst'] = int.from_bytes(socket.inet_pton(socket.AF_INET, args.dst), 'big')
+		tc_filter['src'] = int.from_bytes(socket.inet_pton(socket.AF_INET, args.src), 'big')
+		tc_filter['dst'] = int.from_bytes(socket.inet_pton(socket.AF_INET, args.dst), 'big')
 	except OSError as e:
 		print(f"IP is not valid: {e}")
 		return None
 
-	if args.port not in range(1, 65535):
+	if args.port and args.port not in range(1, 65535):
 		print(f"Port is not valid: {args.port}")
 		return None
 
-	filter['port'] = args.port
+	tc_filter['port'] = args.port
 
-	return filter
+	return tc_filter
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
 		description="Show network packet path through Linux kernel")
-
-	parser.add_argument('--proto',
-						choices=['icmp', 'tcp', 'udp'],
-						type=str,
-						required=True,
-						help='IP protocol to filter')
-	parser.add_argument('--src',
-						type=str,
-						required=True,
-						help="Source IP address (IPv4)")
-	parser.add_argument('--dst',
-						type=str,
-						required=True,
-						help="Destination IP address (IPv4)")
-	parser.add_argument('--port',
-						type=int,
-						help="Port number of TCP/UDP. Source or destination")
+	
+	parser.add_argument(
+		'--link',
+		type=str,
+		required=True,
+		help='Network interface where packet is expected')
+	parser.add_argument(
+		'--proto',
+		choices=['icmp', 'tcp', 'udp'],
+		type=str,
+		required=True,
+		help='IP protocol to filter')
+	parser.add_argument(
+		'--src',
+		type=str,
+		required=True,
+		help="Source IP address (IPv4)")
+	parser.add_argument(
+		'--dst',
+		type=str,
+		required=True,
+		help="Destination IP address (IPv4)")
+	parser.add_argument(
+		'--port',
+		type=int,
+		help="Port number of TCP/UDP. Source or destination")
 
 	args = parser.parse_args()
-	filter = parse_filter(args)
+	tc_filter = parse_tc_filter(args)
 
-	if filter is None:
+	if not tc_filter:
 		exit(1)
 
-	print(filter)
+	print(tc_filter)
+
+	tc_generate_and_load(tc_filter)
 
--- tc_bpf_prog.py
+++ tc_bpf_prog.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python
 
 import pyroute2
+import socket
+
 from bcc import BPF
 
-prog="""
+tc_prog="""
 #include <uapi/linux/if_ether.h>
 #include <uapi/linux/ip.h>
+#include <uapi/linux/tcp.h>
+#include <uapi/linux/udp.h>
+
 #include <uapi/linux/bpf.h>
 #include <uapi/linux/pkt_cls.h>
 
-#define MAP_KEY_SIZE  4
-
-#define SKB_VAL_SIZE  sizeof(void *)
-#define SKB_VAL_AMT   1
-#define PATH_VAL_SIZE sizeof(uint64_t)
-#define PATH_VAL_AMT  128
-
-#define FILTER_IP_PROTO IPPROTO_ICMP
-#define FILTER_SRC_IP   0xAC1F000A /* 172.31.0.10 */
-
-#define TC_NUM  0
-#define TC_NAME "__netif_receive_skb_core"
-
-BPF_ARRAY(skb_ptr, void*, 1);
+BPF_TABLE_PUBLIC("array", u32, void*, skb_ptr, 1);
 
 int filter(struct __sk_buff *skb)
 {
@@ -38,30 +30,32 @@ int filter(struct __sk_buff *skb)
 	if (*skb_val != 0)    return TC_ACT_OK;
 
 	/* Get header pointers for filtering */
-	void         *data = (void *)(long)skb->data;
-	void     *data_end = (void *)(long)skb->data_end;
-	struct ethhdr *eth = data;
-	struct iphdr  *iph = data + sizeof(*eth);
-	/* Bounds check */
-	if (data + sizeof(*eth) + sizeof(*iph) > data_end) return TC_ACT_OK;
+	void          *data = (void *)(long)skb->data;
+	void      *data_end = (void *)(long)skb->data_end;
+	struct ethhdr  *eth = data;
+	struct iphdr   *iph = data + sizeof(*eth);
+	struct tcphdr *tcph = (void *)iph + sizeof(*iph);
+	struct udphdr *udph = (void *)iph + sizeof(*iph);
 
 	/* Return TC_ACT_UNSPEC to allow other filters process this packet
 	 * Return TC_ACT_OK to stop all TC actions and continue
 	 * packet proces by networking stack
 	 */
-	
-	// PROGRAMMABLE_BPF_FILTER
-	//  /* Check IPv4 */
-	// if (eth->h_proto != htons(ETH_P_IP)) return TC_ACT_UNSPEC;
-	// /* Check IP protocol */
-	// if (iph->protocol != FILTER_IP_PROTO) return TC_ACT_UNSPEC;
-	// /* Check src IP address */
-	// if (iph->saddr != htonl(FILTER_SRC_IP)) return TC_ACT_UNSPEC;
+	TRANS_HDR_BOUNDS_CHECK
 
-	/* That is probably wrong. This value should be stored properly */
-	/* *skb_val = (void *)skb; */
-	void *skb_ptr_store = (void *)skb;
-	skb_ptr.update(&skb_key, &skb_ptr_store);
+	/* Check IPv4, if not -- stop all filtering */
+	if (eth->h_proto != htons(ETH_P_IP)) return TC_ACT_OK;
+
+	IP_PROTO_FILTER
+
+	SRC_IP_FILTER
+
+	DST_IP_FILTER
+
+	PORT_FILTER
+
+	//void *skb_ptr_store = (void *)skb;
+	skb_ptr.update(&skb_key, (void **)&skb);
 
 	u64 timestamp = bpf_ktime_get_ns();
 	bpf_trace_printk("Timestamp: %llu", timestamp);
@@ -70,20 +64,61 @@ int filter(struct __sk_buff *skb)
 }
 """
 
-b = BPF(text=prog)
-bpf_obj = b.load_func("filter", BPF.SCHED_CLS)
+def tc_generate(tc_filter):
+	global tc_prog
+	# Check IP protocol
+	filter_proto = tc_filter['proto']
+	tc_prog = tc_prog.replace(
+		'IP_PROTO_FILTER',
+		f"if (iph->protocol != {filter_proto}) return TC_ACT_UNSPEC;")
 
-print(bpf_obj.fd)
-print(bpf_obj.name)
+	# Check source IP address
+	filter_src_ip = tc_filter['src']
+	tc_prog = tc_prog.replace(
+		'SRC_IP_FILTER',
+		f"if (iph->saddr != htonl({filter_src_ip})) return TC_ACT_UNSPEC;")
 
-ipr = pyroute2.IPRoute()
-link = ipr.link_lookup(ifname="ens37")[0]
-ipr.tc("add", "clsact", link)
-ipr.tc("add-filter", "bpf", link, fd=bpf_obj.fd, name=bpf_obj.name,
-           parent="ffff:fff2", direct_action=True)
+	# Check destination IP address
+	filter_dst_ip = tc_filter['dst']
+	tc_prog = tc_prog.replace(
+		'DST_IP_FILTER',
+		f"if (iph->daddr != htonl({filter_dst_ip})) return TC_ACT_UNSPEC;")
+	
+	if tc_filter['port'] and tc_filter['proto'] is not socket.IPPROTO_ICMP:
+		filter_trans_hdr = "tcph" if tc_filter['proto'] is socket.IPPROTO_TCP else "udph"
+		filter_port = tc_filter['port']
 
-try:
-	while True:
+		# Bounds check to filter out small packets
+		tc_prog = tc_prog.replace(
+			'TRANS_HDR_BOUNDS_CHECK',
+			f"if (data + sizeof(*eth) + sizeof(*iph) + sizeof(*{filter_trans_hdr}) > data_end) return TC_ACT_OK;")
+
+		# Check if either source or dest port fits
+		tc_prog = tc_prog.replace(
+			'PORT_FILTER',
+			f"if ({filter_trans_hdr}->source != htons({filter_port}) && {filter_trans_hdr}->dest != htons({filter_port})) return TC_ACT_UNSPEC;")
+	else:
+		# Without transport header filtering check bounds only till IP header
+		tc_prog = tc_prog.replace(
+			'TRANS_HDR_BOUNDS_CHECK',
+			"if (data + sizeof(*eth) + sizeof(*iph) > data_end) return TC_ACT_OK;")
+		tc_prog = tc_prog.replace('PORT_FILTER', "")
+
+	print(tc_prog)
+
+def tc_generate_and_load(tc_filter):
+	tc_generate(tc_filter)
+
+	b = BPF(text=tc_prog)
+	bpf_obj = b.load_func("filter", BPF.SCHED_CLS)
+
+	link = tc_filter['link']
+	ipr = pyroute2.IPRoute()
+	ipr.tc("add", "clsact", link)
+	ipr.tc("add-filter", "bpf", link, ":1", fd=bpf_obj.fd, name=bpf_obj.name,
+		   parent="ffff:fff2", direct_action=True)
+
+	try:
 		b.trace_print()
-except KeyboardInterrupt:
-	ipr.tc("del", "clsact", link)
+	except KeyboardInterrupt:
+		ipr.tc("del", "clsact", link)
